6a1e036798b9bfa74bab75795f8bb9aa56e41462
Fro116 <kundanc@mit.edu>
2017-12-20 22:07:53 -0500

    Initial Commit

---
+++ InteractiveBackground/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InteractiveBackground
+//
+//  Created by Kundan Chintamaneni on 12/20/17.
+//  Copyright © 2017 Kundan Chintamaneni. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ InteractiveBackground/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InteractiveBackground
+//
+//  Created by Kundan Chintamaneni on 12/20/17.
+//  Copyright © 2017 Kundan Chintamaneni. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
