f4d39c2fe6dfd0ecaff5fb5fad75e4a6146f47c1
Fro116 <kundanc@mit.edu>
2017-12-29 16:12:29 -0500

    Window now mirrors the desired app.

--- InteractiveBackground/DesktopWindow.swift
+++ InteractiveBackground/DesktopWindow.swift
@@ -13,6 +13,9 @@ class DesktopWindow : NSWindow {
     
     override init(contentRect: NSRect, styleMask: NSWindow.StyleMask, backing: NSWindow.BackingStoreType, defer _defer: Bool) {
         super.init(contentRect: contentRect, styleMask: styleMask, backing: backing, defer: _defer)
+        // display above desktop background and behind desktop icons
+        let desktopLevel = Int(CGWindowLevelForKey(CGWindowLevelKey.desktopWindow))
+        level = NSWindow.Level(desktopLevel)
     }
     
 }
--- InteractiveBackground/ViewController.swift
+++ InteractiveBackground/ViewController.swift
@@ -9,11 +9,43 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    private func isBackgroundEvent(event: NSEvent) -> Bool {
+        // checks that all windows the point intersects with are below the background level
+        let visibleLevel = CGWindowLevel(CGWindowLevelKey.baseWindow.rawValue)
+        let backgroundLevel = CGWindowLevel(CGWindowLevelKey.desktopIconWindow.rawValue)
+        let location = event.locationInWindow
+        return WindowInfo.getWindowList(listOptions: CGWindowListOption.optionOnScreenOnly)
+            .filter({$0.windowLevel() >= visibleLevel})
+            .filter({$0.bounds().contains(location) == true})
+            .map({$0.windowLevel() == backgroundLevel})
+            .reduce(true, {$0 && $1})
+    }
+    
+    private func handleEvent(event: NSEvent) {
+        // forward events that interact with the desktop background
+        if isBackgroundEvent(event: event) {
+            handler.handle(event: event)
+        }
+    }
+    
+    @objc private func updateImage() {
+        if let image = handler.image() {
+            imageView!.image = image
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // set size to be full screen
         self.view.frame = NSScreen.main!.frame
+        // set up event forwarding
+        let mask : NSEvent.EventTypeMask = [NSEvent.EventTypeMask.leftMouseDown, NSEvent.EventTypeMask.leftMouseUp]
+        NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handleEvent)
+        // set up image mirroring
+        imageView = NSImageView(frame: self.view.frame)
+        self.view.addSubview(imageView!)
+        Timer.scheduledTimer(timeInterval: frameRate, target: self, selector: #selector(updateImage), userInfo: nil, repeats: true)
     }
 
     override var representedObject: Any? {
@@ -22,6 +54,8 @@ class ViewController: NSViewController {
         }
     }
 
-
+    private let handler : ApplicationInterface = MonikaAfterstoryAdapter()
+    private let frameRate = 0.05
+    private var imageView : NSImageView? = nil
 }
 
