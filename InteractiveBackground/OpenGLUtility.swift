b40c9dcf7a90b630e8e575d8db3373b7c1a53543
Fro116 <kundanc@mit.edu>
2018-01-07 02:37:07 -0500

    Imported OpenGL drawing classes.

---
+++ InteractiveBackground/OpenGLUtility.swift
@@ -0,0 +1,152 @@
+//
+//  OpenGLUtility.swift
+//  InteractiveBackground
+//
+//  Created by Kundan Chintamaneni on 1/1/18.
+//  Copyright © 2018 Kundan Chintamaneni. All rights reserved.
+//
+
+import Cocoa
+import OpenGL.GL3
+
+final class OpenGLUtility {
+    
+    /**
+     * Initializes an OpenGL context
+     *
+     * - returns: an OpenGL context, if one could be created
+     */
+    public static func createContext() -> NSOpenGLContext? {
+        let attrs: [NSOpenGLPixelFormatAttribute] = [
+            UInt32(NSOpenGLPFAAccelerated),
+            UInt32(NSOpenGLPFADoubleBuffer),
+            UInt32(NSOpenGLPFAColorSize), UInt32(32),
+            UInt32(NSOpenGLPFAOpenGLProfile), UInt32(NSOpenGLProfileVersion3_2Core),
+            UInt32(0)
+        ]
+        guard let pixelFormat = NSOpenGLPixelFormat(attributes: attrs) else {
+            Swift.print("pixelFormat could not be constructed")
+            return nil
+        }
+        guard let context = NSOpenGLContext(format: pixelFormat, share: nil) else {
+            Swift.print("context could not be constructed")
+            return nil
+        }
+        return context
+    }
+    
+    /**
+     * Sets up an OpenGL program
+     *
+     * - parameter vertexShader: the source text of the vertex shader file
+     * - parameter fragmentShader: the source text of the fragment shader file
+     * - returns: the handle to the program
+     */
+    public static func createProgram(vertexShader : URL, fragmentShader : URL) -> GLuint {
+        let programID = glCreateProgram()
+        
+        let vertexType = GLenum(GL_VERTEX_SHADER)
+        let vertexSource = try! String(contentsOf: vertexShader, encoding: String.Encoding.ascii)
+        OpenGLUtility.attachShader(text: vertexSource, shaderType: vertexType, programID : programID)
+        
+        let fragmentType = GLenum(GL_FRAGMENT_SHADER)
+        let fragmentSource = try! String(contentsOf: fragmentShader, encoding: String.Encoding.ascii)
+        OpenGLUtility.attachShader(text: fragmentSource, shaderType: fragmentType, programID: programID);
+        
+        OpenGLUtility.linkProgram(programID: programID)
+        return programID
+    }
+    
+    /**
+     * Loads the image into a texture
+     *
+     * - parameter image: the texture data
+     * - returns: a handle to the texture
+     */
+    public static func loadTexture(image : CGImage) -> GLuint {
+        var tboID: GLuint = 0
+
+        let bitsInByte = 8;
+        let numBytes = image.bitsPerPixel / bitsInByte * image.width * image.height
+        let textureData = UnsafeMutableRawPointer.allocate(bytes: numBytes, alignedTo: MemoryLayout<GLint>.alignment)
+        let context = CGContext(data: textureData, width: image.width, height: image.height, bitsPerComponent: bitsInByte, bytesPerRow: image.bytesPerRow, space: CGColorSpace(name: CGColorSpace.sRGB)!, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)
+        context?.draw(image, in: CGRect(x: 0.0, y: 0.0, width: Double(image.width), height: Double(image.height)))
+        
+        glGenTextures(1, &tboID)
+        glBindTexture(GLenum(GL_TEXTURE_2D), tboID)
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GL_LINEAR)
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MAG_FILTER), GL_LINEAR)
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GL_CLAMP_TO_EDGE)
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_T), GL_CLAMP_TO_EDGE)
+        glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GL_RGBA, GLsizei(image.width), GLsizei(image.height), 0, GLenum(GL_RGBA), GLenum(GL_UNSIGNED_BYTE), textureData)
+        glBindTexture(GLenum(GL_TEXTURE_2D), 0)
+        free(textureData)
+        
+        return tboID
+    }
+    
+    /**
+     * Links the program after shaders have been attached
+     *
+     * - parameter programID: a handle to the program
+     */
+    private static func linkProgram(programID : GLuint) {
+        // link program
+        glLinkProgram(programID)
+        var linked: GLint = 0
+        glGetProgramiv(programID, UInt32(GL_LINK_STATUS), &linked)
+        
+        // test for errors
+        if linked <= 0 {
+            Swift.print("Could not link, getting log")
+            var logLength: GLint = 0
+            glGetProgramiv(programID, UInt32(GL_INFO_LOG_LENGTH), &logLength)
+            Swift.print(" logLength = \(logLength)")
+            if logLength > 0 {
+                let cLog = UnsafeMutablePointer<CChar>.allocate(capacity: Int(logLength))
+                glGetProgramInfoLog(programID, GLsizei(logLength), &logLength, cLog)
+                Swift.print("Program Error Log: \(String.init(cString: cLog))")
+                free(cLog)
+            }
+        }
+    }
+    
+    /**
+     * Attaches a shader to the given program
+     *
+     * -parameter text: the source text of the shader file
+     * -parameter shaderType: either GL_FRAGMENT_SHADER or GL_VERTEX_SHADER
+     * -parameter programID: a handle to the program
+     */
+    private static func attachShader(text : String, shaderType : GLenum, programID : GLuint) {
+        // compile shader
+        let vs = glCreateShader(shaderType)
+        let vss = text.cString(using: String.Encoding.ascii)
+        var vssptr = UnsafePointer<GLchar>(vss)
+        glShaderSource(vs, 1, &vssptr, nil)
+        glCompileShader(vs)
+        
+        // test for errors
+        var compiled: GLint = 0
+        glGetShaderiv(vs, GLbitfield(GL_COMPILE_STATUS), &compiled)
+        if compiled <= 0 {
+            Swift.print("Could not compile, getting log")
+            var logLength: GLint = 0
+            glGetShaderiv(vs, GLenum(GL_INFO_LOG_LENGTH), &logLength)
+            Swift.print(" logLength = \(logLength)")
+            if logLength > 0 {
+                let cLog = UnsafeMutablePointer<CChar>.allocate(capacity: Int(logLength))
+                glGetShaderInfoLog(vs, GLsizei(logLength), &logLength, cLog)
+                Swift.print("Vert Error Log = \(String.init(cString: cLog))")
+                free(cLog)
+            }
+        }
+        
+        // link shader
+        glAttachShader(programID, vs)
+        glDeleteShader(vs)
+    }
+    
+}
+
+
---
+++ InteractiveBackground/SwiftOpenGLView.swift
@@ -0,0 +1,100 @@
+//
+//  SwiftOpenGLView.swift
+//  InteractiveBackground
+//
+//  Created by Kundan Chintamaneni on 1/1/18.
+//  Copyright © 2018 Kundan Chintamaneni. All rights reserved.
+//
+
+
+import Cocoa
+import OpenGL.GL3
+
+/**
+ * An animated view context that displays a single image
+ */
+final class SwiftOpenGLView: NSOpenGLView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        openGLContext = OpenGLUtility.createContext()
+        pixelFormat = openGLContext?.pixelFormat
+        openGLContext?.setValues([1], for: .swapInterval)
+        
+        let bounds = CGRect(x: -1, y: -1, width: 2, height: 2)
+        display = OpenGLRectangle(bounds: bounds)
+    }
+    
+    override func prepareOpenGL() {
+        super.prepareOpenGL()
+        
+        configureOpenGL()
+        startAnimation()
+    }
+    
+    /**
+     * Sets the OpenGL rendering parameters, such as the
+     * shaders and the clear color
+     */
+    private func configureOpenGL() {
+        let vertexSource = Bundle.main.url(forResource: "Texture", withExtension: "vertexshader", subdirectory: "")!
+        let fragmentSource = Bundle.main.url(forResource: "Texture", withExtension: "fragmentshader", subdirectory: "")!
+        programID = OpenGLUtility.createProgram(vertexShader: vertexSource, fragmentShader: fragmentSource)
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+    }
+    
+    /**
+     * Creates and starts a DisplayLink. This causes `drawView` to
+     * be called whenever a frame will be drawn
+     */
+    private func startAnimation() {
+        let displayLinkOutputCallback: CVDisplayLinkOutputCallback = {(displayLink: CVDisplayLink, inNow: UnsafePointer<CVTimeStamp>, inOutputTime: UnsafePointer<CVTimeStamp>, flagsIn: CVOptionFlags, flagsOut: UnsafeMutablePointer<CVOptionFlags>, displayLinkContext: UnsafeMutableRawPointer?) -> CVReturn in
+            
+            let view = unsafeBitCast(displayLinkContext, to: SwiftOpenGLView.self)
+            view.drawView()
+            return kCVReturnSuccess
+        }
+        
+        CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+        CVDisplayLinkSetOutputCallback(displayLink!, displayLinkOutputCallback, UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque()))
+        CVDisplayLinkStart(displayLink!)
+    }
+    
+    /**
+     * Renders the scene.
+     */
+    private func drawView() {
+        if let context = self.openGLContext {
+            context.makeCurrentContext()
+            CGLLockContext(context.cglContextObj!)
+            glUseProgram(programID)
+            
+            glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+            display?.draw()
+            
+            glUseProgram(0)
+            CGLFlushDrawable(context.cglContextObj!)
+            CGLUnlockContext(context.cglContextObj!)
+        }        
+    }
+
+    /**
+     * Clean up OpenGL resources
+     */
+    deinit {
+        glDeleteProgram(programID)
+        CVDisplayLinkStop(displayLink!)
+    }
+    
+    /// a handle to the program shaders
+    private var programID: GLuint = 0
+    
+    /// the display link, which is used for animations
+    private var displayLink: CVDisplayLink?
+    
+    /// a frame that will render the current image
+    private var display: OpenGLRectangle?
+    
+}
+
